d0f2a8bdf8de9ad7e7d596ddec7ecb9ad5679986
Szabó László András <noreply@zza.hu>
2021-05-22 23:57:10 +0200

    Adopt clim 0.3.0 + 'class file for com.google.gson.JsonObject not found'

---
+++ module-info.java
@@ -0,0 +1,9 @@
+module Shared.Bills.Splitter.Hyperskill.main {
+  requires clim;
+  requires java.persistence;
+  requires spring.boot;
+  requires spring.boot.autoconfigure;
+  requires spring.beans;
+  requires spring.context;
+  requires spring.data.commons;
+}
