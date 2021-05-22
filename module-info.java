466587af9c2d8fd13ad1d7ac4a971bfaaf410199
Szabó László András <noreply@zza.hu>
2021-05-23 00:25:26 +0200

    A great lesson... 'class file for com.google.gson.JsonObject not found' Provide each and every requirements. Parameters make dependencies too.

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module Shared.Bills.Splitter.Hyperskill.main {
-  requires clim;
-  requires java.persistence;
-  requires spring.boot;
-  requires spring.boot.autoconfigure;
-  requires spring.beans;
-  requires spring.context;
-  requires spring.data.commons;
-}
